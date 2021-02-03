35d97d475a90b53ca7a1d48e2a29df7ce6608972
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-02-02 20:54:00 -0500

    Update Library.java

--- src/pkg/Library.java
+++ src/pkg/Library.java
@@ -1,5 +1,7 @@
 package pkg;
 
+import java.util.Arrays;
+
 public class Library {
     private Book[] books; // array-based implementation of the bag data structure
     private int numBooks; // the number of books currently in the bag
@@ -8,9 +10,19 @@ public class Library {
         this.books = new Book[4];
         this.numBooks = 0;
     }
-    private int find(Book book) { return 0;} // helper method to find a book in the bag
-    private void grow() { } // helper method to grow the capacity by 4
-    public void add(Book book) { }
+    private int find(Book book) { // helper method to find a book in the bag
+        return 0;
+    }
+    private void grow() { // helper method to grow the capacity by 4
+        Book [] newBooks = Arrays.copyOf(this.books,this.books.length+4);
+        
+    }
+    public void add(Book book) {
+        if(numBooks == books.length){
+            this.grow();
+        }
+        books[++numBooks] = book;
+    }
     public boolean remove(Book book) { return false;}
     public boolean checkOut(Book book) { return false;}
     public boolean returns(Book book) { return false;}
