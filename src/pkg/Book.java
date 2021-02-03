fba2a75b3ecb480d080ec3e93bc42a578073e631
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-02-02 19:43:24 -0500

    started coding Book and Date

--- src/pkg/Book.java
+++ src/pkg/Book.java
@@ -6,15 +6,18 @@ public class Book {
     private boolean checkedOut;
     private Date datePublished;
 
-
+    @Override
     public boolean equals(Object obj){
-
+        Book newBook = (Book)obj;
+        if(this.number.equals(newBook.number)){
+            return true;
+        }
         return false;
     }
 
     @Override
     public String toString() {
 
-        return null;
+        return "Book"+this.number+"::"+this.name+"::";
     }
 }
--- src/pkg/Date.java
+++ src/pkg/Date.java
@@ -4,7 +4,11 @@ public class Date {
     private int year;
     private int month;
     private int day;
-    public Date(String date) { } //taking mm/dd/yyyy and create a Date object
+    public Date(String date) { //taking mm/dd/yyyy and create a Date object
+        this.month = Integer.valueOf(date.substring(0,2));
+        this.day = Integer.valueOf(date.substring(3,5));
+        this.year = Integer.valueOf(date.substring(6));
+    }
     public Date() { } //return today’s date
 
     //public isValid() { }
