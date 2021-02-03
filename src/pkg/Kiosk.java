dc92d796a0ceca6980a087691b8f3f2dcb873f31
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-02-02 20:04:50 -0500

    Update Kiosk.java

--- src/pkg/Kiosk.java
+++ src/pkg/Kiosk.java
@@ -1,8 +1,31 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class Kiosk {
 
     public void run(){
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+        while(!input.equals("Q")){
+            if(input.equals("PA")){
+
+            }else if(input.equals("PD")){
+
+            }else if(input.equals("PN")){
+
+            }else if(input.charAt(0) == 'A'){
+
+            }else if(input.charAt(0) == 'R'){
+
+            }else if(input.charAt(0) == 'O'){
+
+            }else if(input.charAt(0) == 'I'){
+                
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
