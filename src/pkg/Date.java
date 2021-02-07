6f9d002c6644a234c7f92427417bd7ba7868df07
Prasidh Sriram <prasidh.sw1ft@gmail.com>
2021-02-07 16:18:30 -0500

    added getters

--- src/pkg/Date.java
+++ src/pkg/Date.java
@@ -1,5 +1,7 @@
 package pkg;
 
+import java.util.Calendar;
+
 public class Date {
     private int year;
     private int month;
@@ -10,7 +12,25 @@ public class Date {
         this.day = Integer.valueOf(date.substring(3,5));
         this.year = Integer.valueOf(date.substring(6));
     }
-    public Date() { } //return today’s date
+    public Date() { //return today’s date
+        Calendar calendar = Calendar.getInstance();
+
+        year = calendar.get(calendar.YEAR);
+        month = calendar.get(calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
 
     //public isValid() { }
 }
